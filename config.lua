98ce4c43479813615bcea2fcfc811bc7d1d55752
JoeV2 <146123038+Joe-Development@users.noreply.github.com>
2024-04-10 05:43:34 -0500

    ⭐  New Gifs
    ⭐  New Gifs

--- config.lua
+++ config.lua
@@ -11,17 +11,18 @@ Config = {
 	KeyBind = 289, -- F2 -- USE https://docs.fivem.net/docs/game-references/controls/ for keycodes
 	roleList = { 
 		{'0', '~w~Civillian '},
+		{'1147775268577087549', "~o~Co Owner "}, -- -- done
 		{'1147775268577087549', "~p~Owner "}, -- -- done
 	},
 
 	-- Headtag Menu Stuff
-	customMenuTexture = false,
-	menutexture_fileName = 'custommenu', -- will only work if [ customMenuTexture ] is set to true
+	useHeadTagMenuImage = true,
+	HeadTagMenuImage = 'https://cdn.discordapp.com/attachments/1161069645827166304/1227568064170819676/tumblr_68144a71c3035f7dc7aa8aafa9d21e78_fd150a88_2048.gif?ex=6628e0d9&is=66166bd9&hm=3b2da5214ba65c7db3aec1850a231661773ec12d35b5397ee29cbed3598e770d&', -- [Custom banner IMGUR or GIPHY URLs go here (Includes Discord Image URLS) ]
 	playerNameTitle = false,
-	headTagMenuTitle = "~b~HeadTag ~y~Menu", -- only work if [ playerNameTitle ] is set to false
+	headTagMenuTitle = "", -- only work if [ playerNameTitle ] is set to false
 	MenuPos = {
-		x = 1400,
-		y = 100
+		x = 1450,
+		y = 200
 	},
 	commandInfo = {
 		command = 'headtags',
